df0752fb36830c5c6a4e81edd634825a2df09a6e
Alisson P. <alisonsilva199500@gmail.com>
2025-07-02 22:57:12 -0300

    testando

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
-import CadUserPage from './pages/CadUserPage'
-import HomePage from './pages/HomePage'
-import Apiarios from './pages/Apiarios'
 import AppRoutes from './routes'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    
-    {/* <CadUserPage />
-    <HomePage />
-    <Apiarios /> */}
-   <AppRoutes/> 
-   
-  </StrictMode>,
+  <StrictMode>        
+   <AppRoutes/>    
+  </StrictMode>
 )
